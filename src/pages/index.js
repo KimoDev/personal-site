67800734350454f893eb21bbb76b7097504501f4
Kimo <jm865@kent.ac.uk>
2019-11-06 22:44:54 +0000

    Changed index scss file to scss module file to resolve classname conflicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Overlay from '../components/Overlay';
 import FooterOverlay from '../components/FooterOverlay';
 
 
-import '../styles/index.scss';
+import styles from '../styles/index.module.scss';
 import '../styles/global.scss';
 
 const HomePage = () => {
@@ -19,13 +19,13 @@ const HomePage = () => {
   const renderOverlay = () => {
     return (
       
-        <animated.div className="main" style={fade}>
-        <div className="container">
+        <animated.div className={styles.main} style={fade}>
+        <div className={styles.container}>
         <Header 
         navClicked={() => toggleNav(!nav)} 
         icon={require('../images/icons/close-icon.svg')}/>
             <Overlay nav={nav}/>
-        <div className="footer">
+        <div className={styles.footer}>
           <FooterOverlay />
         </div>
           </div>
@@ -37,20 +37,20 @@ const HomePage = () => {
   return (
     <div>
     {nav ? renderOverlay() : null }
-      <div className="container">
+      <div className={styles.container}>
       
       <Header 
       navClicked={() => toggleNav(!nav)} 
       icon={require('../images/icons/burger-menu.svg')}/>
       
-      <div className="wrapper">
+      <div className={styles.wrapper}>
 
-        <div className="content-left" >
+        <div className={styles.contentLeft} >
         
-          <h1 className="title">Hi, I’m Kimo.I'm A Developer, Designer & Content Creator</h1>
-          <p className="intro">A 21 year old Computer Science Student, currently in my 
+          <h1 className={styles.title}>Hi, I’m Kimo.I'm A Developer, Designer & Content Creator</h1>
+          <p className={styles.intro}>A 21 year old Computer Science Student, currently in my 
           final year studying at the University of Kent.</p>
-          <p className="intro">As a self-taught individual for 7+ years, I have strengthen
+          <p className={styles.intro}>As a self-taught individual for 7+ years, I have strengthen
           my skills and developed interests over a wide range of 
           areas. 
             
@@ -63,19 +63,17 @@ const HomePage = () => {
           
         </div>
 
-        <div className="content-right">
+        <div className={styles.contentRight}>
           {/*<img src={require('../assets/kimodev.jpg')} alt="Kimo Dev " className="frontImg"/> */}
-          <div className="frontImg"></div>
+          <div className={styles.frontImg}></div>
         </div>
 
         </div>
-        <div className="footer">
+      <div className={styles.footer}>
           <Social />
         </div>
         
     </div>
-    
-   
     
     </div>
     
--- src/pages/project.js
+++ src/pages/project.js
@@ -3,22 +3,22 @@ import React, { useState } from 'react'
 import Header from '../components/Header';
 import ProjectContainer from '../components/ProjectContainer';
 
-import '../styles/projects.scss';
+import styles from '../styles/projects.scss';
 
 const project = () => {
   // const [nav, toggleNav] = useState(false);
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Header 
       // navClicked={() => toggleNav(!nav)} 
       icon={require('../images/icons/burger-menu.svg')}/>
-        <h1>Projects I've been working on</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Cum fugit aliquid quaerat voluptatum nulla in facere laudantium 
-        reiciendis natus quia voluptates consequuntur cupiditate expedita, 
-        nam aperiam illum, perspiciatis cumque. Maxime?
-        </p>
+      <h1>Projects I've been working on</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+      Cum fugit aliquid quaerat voluptatum nulla in facere laudantium 
+      reiciendis natus quia voluptates consequuntur cupiditate expedita, 
+      nam aperiam illum, perspiciatis cumque. Maxime?
+      </p>
       <div className="project-row">
         <ProjectContainer />
         <ProjectContainer />
